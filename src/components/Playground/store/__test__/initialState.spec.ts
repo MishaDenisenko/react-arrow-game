4e4a1049561e2280b3322542fd7dc44d572a7c0a
mihailden <midenisenko@gmail.com>
2023-11-19 19:22:13 +0200

    Updates

--- src/components/Playground/store/__test__/initialState.spec.ts
+++
@@ -1,9 +0,0 @@
-import playgroundReducer, { initialState } from '../slice';
-
-describe('initial state', function () {
-    it('should check initial state', function () {
-        const setInitialState = playgroundReducer(undefined, {type: 'unknown'});
-
-        expect(setInitialState).toEqual(initialState);
-    });
-})
